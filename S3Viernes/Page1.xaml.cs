684bc2250737653fb86cbfe157847ec19ed96b06
master <master@SMLAB310PC03_23>
2023-06-19 19:29:56 -0500

    Agregar archivos de proyecto.

---
+++ S3Viernes/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3Viernes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicio_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "uisrael";
+            string contrasenia = "2023";
+            if (txtUsuario.Text == usuario && txtContrasenia.Text == contrasenia)
+            {
+                Navigation.PushAsync(new Page2(usuario));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario o contraseña incorrectos", "Cerrar");
+            }
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ S3Viernes/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3Viernes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        string Usuario;
+        public Page2(string usuario)
+        {
+            InitializeComponent();
+            this.Usuario = usuario;
+            lblUsuario.Text = usuario;
+        }
+
+        private void txtDato_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+
+                double dato = Convert.ToDouble(txtDato.Text);
+                if (dato > 100)
+                {
+                    DisplayAlert("Error", "El dato no está en rango", "Cerrar");
+                    txtDato.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                //DisplayAlert("Muerte", "Muerto amiguito :c" + ex, "Cerrar");
+            }
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            double dato = Convert.ToDouble(txtDato.Text);
+            string alumno  = pkAlumnos.Items[pkAlumnos.SelectedIndex];
+            Navigation.PushAsync(new Page3(this.Usuario, dato, alumno));
+        }
+    }
+}
